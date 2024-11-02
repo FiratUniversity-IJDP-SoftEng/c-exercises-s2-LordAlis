d8c5a7cf3dd5a661b60a81c5c8726af087902be9
LordAlis <alicagancebeci@gmail.com>
2024-11-03 00:28:38 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int arr[17]; 
+    int index = 0;
+
+    for (int i = 99; i >= 51; i -= 3) {  
+        arr[index++] = i; 
+    }
+
+    for (int i = 0; i < 17; i++) {
+        printf("%d", arr[i]);
+        if (i < 16) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
