e03500714bf843bab76d89238907f86502704424
LordAlis <alicagancebeci@gmail.com>
2024-11-04 16:38:11 +0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+int main() {
+    int rows = 5;  
+
+    for (int i = 1; i <= rows; i++) {
+        // print code
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+
+        // Print of the increasing
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Print of the decreasing
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    // The lower part of the diamond shape 
+    for (int i = rows - 1; i >= 1; i--) {
+        // Printing the leading places
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+
+        // Print of the increasing
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Print of the decreasing
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
 
