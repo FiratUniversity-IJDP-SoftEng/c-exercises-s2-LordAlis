54a6a1e51289a0682c319efa0603e30ed025a53f
LordAlis <alicagancebeci@gmail.com>
2024-11-04 15:27:48 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int number, count = 0, sum = 0;
+    float average;
+
+    printf("Enter numbers (enter -1 to stop):\n");
+    while (1) {
+        scanf("%d", &number);
+        if (number == -1) break;
+        sum += number;
+        count++;
+    }
+
+    if (count > 0) {
+        average = sum / (float)count;
+        printf("Total numbers entered: %d\nSum: %d\nAverage: %.2f\n", count, sum, average);
+    } else {
+        printf("No numbers other than -1 entered.\n");
+    }
+
+    return 0;
+}
 
