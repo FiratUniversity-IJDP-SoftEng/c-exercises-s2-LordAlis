162cc4976d7ca66193a2e60065517ee2eff9819d
LordAlis <alicagancebeci@gmail.com>
2024-11-04 15:15:34 +0300

    Update ex08.c
    sum (total) and average working and compiled

--- ex08.c
+++ ex08.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main() {
+    int numbers[10], sum = 0;
+    float average;
+    printf("Enter 10 numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+    average = sum / 10.0;
+    printf("Numbers entered: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\nSum: %d\nAverage: %.2f\n", sum, average);
+    return 0;
+}
