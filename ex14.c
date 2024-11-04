18b2d2df960acc9d13a02bbdee5dc46602a86f20
LordAlis <alicagancebeci@gmail.com>
2024-11-04 16:25:55 +0300

    Update ex14.c
    Fibonacci series calculator with recursion. it works for positive numbers, and compute on it own after your imput

--- ex14.c
+++ ex14.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+
+    printf("Enter the nth position for Fibonacci series: ");
+    scanf("%d", &n);
+
+
+    printf("Fibonacci series up to %d-th position:\n", n);
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+
+    return 0;
+}
 
