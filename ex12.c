469c95115393c9627ae9e88e989b0156f2f55741
LordAlis <alicagancebeci@gmail.com>
2024-11-04 16:15:54 +0300

    Update ex12.c
    I used Unsigned long to store big numbers,  and used Iterative way

--- ex12.c
+++ ex12.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int number;
+    unsigned long factorial = 1;
+
+    printf("Enter a number less than 50: ");
+    scanf("%d", &number);
+
+    if (number < 0 || number >= 50) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d is %llu\n", number, factorial);
+    return 0;
+}
