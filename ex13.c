272a1b81497937263d5fceab18b99a6765f74f6e
LordAlis <alicagancebeci@gmail.com>
2024-11-04 16:18:31 +0300

    Update ex13.c
    I used Recursive way to write factorial code. It uses more memory and it's slover than Iterative way (at least in computing factorials)

--- ex13.c
+++ ex13.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+unsigned long long factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+    printf("Enter a number less than 50: ");
+    scanf("%d", &number);
+
+    if (number < 0 || number >= 50) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    printf("Factorial of %d is %llu\n", number, factorial(number));
+    return 0;
+}
 
